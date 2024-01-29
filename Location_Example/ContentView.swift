16bb5cb708795ad4dac45306484146cfac184254
LeeeeSuHyeon <tngus0673@naver.com>
2024-01-29 17:40:45 +0900

    시뮬레이터에서 설정한 위도 경도 출력 확인

--- Location_Example/ContentView.swift
+++ Location_Example/ContentView.swift
@@ -8,12 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    // ContentView가 초기화될 때 CoreLocationEx 인스턴스 생성
+   @ObservedObject private var coreLocation = CoreLocationEx()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack{
+//            Text("위도 : \(coreLocation.locationManager.location?.coordinate.latitude ?? 0.0)")
+//            Text("경도 : \(coreLocation.locationManager.location?.coordinate.longitude ?? 0.0)")
+//            Text("고도 : \(coreLocation.locationManager.location?.altitude ?? 0.0)")
+//            Text("수평 정확도 : \(coreLocation.locationManager.location?.verticalAccuracy ?? 0.0)")
+//            Text("수직 정확도 : \(coreLocation.locationManager.location?.horizontalAccuracy ?? 0.0)")
+            Text("위도: \(coreLocation.latitude)")
+            Text("경도: \(coreLocation.longitude)")
+            Text("고도: \(coreLocation.altitude)")
         }
         .padding()
     }
--- Location_Example/CoreLocationEx.swift
+++ Location_Example/CoreLocationEx.swift
@@ -5,33 +5,115 @@
 //  Created by 이수현 on 2024/01/28.
 //
 
-import Foundation
-import UIKit
+import SwiftUI
 import CoreLocation
 
-class ViewController : UIViewController, CLLocationManagerDelegate {
-    var locationManager = CLLocationManager()
+class CoreLocationEx: NSObject, ObservableObject, CLLocationManagerDelegate {
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // 델리게이트 설정
+    @Published var latitude: Double = 0.0
+    @Published var longitude: Double = 0.0
+    @Published var altitude: Double = 0.0
+
+    var locationManager = CLLocationManager()
+
+    override init() {
+        super.init()
+        self.setupLocationManager()
+    }
+
+    func setupLocationManager() {
         locationManager.delegate = self
-        
-        // 거리 정확도 설정
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
-        
-        // 사용자에게 허용 받기 alert 띄우기
         locationManager.requestWhenInUseAuthorization()
-        
-        // 아이폰 설정에서의 위치 서비스가 켜진 상태라면
+
         if CLLocationManager.locationServicesEnabled() {
             print("위치 서비스 On 상태")
-            locationManager.startUpdatingLocation()  // 위치 받아오기 시작
-            print(locationManager.location?.coordinate)
+            locationManager.startUpdatingLocation()
+            print(locationManager.location?.coordinate as Any)
         } else {
             print("위치 서비스 Off 상태")
         }
     }
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last {
+            latitude = location.coordinate.latitude
+            longitude = location.coordinate.longitude
+            altitude = location.altitude
+        }
+    }
+
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        checkUserDeviceLocationServiceAuthorization()
+    }
+
+    func checkUserDeviceLocationServiceAuthorization() {
+        // 3.1
+       guard CLLocationManager.locationServicesEnabled() else {
+           // 시스템 설정으로 유도하는 커스텀 얼럿
+           showRequestLocationServiceAlert()
+           return
+       }
+           
+           
+       // 3.2
+       let authorizationStatus: CLAuthorizationStatus
+           
+       // 앱의 권한 상태 가져오는 코드 (iOS 버전에 따라 분기처리)
+       if #available(iOS 14.0, *) {
+           authorizationStatus = locationManager.authorizationStatus
+       } else {
+           authorizationStatus = CLLocationManager.authorizationStatus()
+       }
+           
+       // 권한 상태값에 따라 분기처리를 수행하는 메서드 실행
+       checkUserCurrentLocationAuthorization(authorizationStatus)
+    }
     
-    
+    // 앱에 대한 위치 권한이 부여된 상태인지 확인하는 메서드
+       func checkUserCurrentLocationAuthorization(_ status: CLAuthorizationStatus) {
+           switch status {
+           case .notDetermined:
+               // 사용자가 권한에 대한 설정을 선택하지 않은 상태
+               
+               // 권한 요청을 보내기 전에 desiredAccuracy 설정 필요
+               locationManager.desiredAccuracy = kCLLocationAccuracyBest
+               
+               // 권한 요청을 보낸다.
+               locationManager.requestWhenInUseAuthorization()
+                   
+           case .denied, .restricted:
+               // 사용자가 명시적으로 권한을 거부했거나, 위치 서비스 활성화가 제한된 상태
+               // 시스템 설정에서 설정값을 변경하도록 유도한다.
+               // 시스템 설정으로 유도하는 커스텀 얼럿
+               showRequestLocationServiceAlert()
+               
+           case .authorizedWhenInUse:
+               // 앱을 사용중일 때, 위치 서비스를 이용할 수 있는 상태
+               // manager 인스턴스를 사용하여 사용자의 위치를 가져온다.
+               locationManager.startUpdatingLocation()
+               
+           default:
+               print("Default")
+           }
+       }
+       
+       
+       
+       // 설정으로 이동하는 알림
+       func showRequestLocationServiceAlert() {
+           let requestLocationServiceAlert = UIAlertController(title: "위치 정보 이용", message: "위치 서비스를 사용할 수 없습니다.\n디바이스의 '설정 > 개인정보 보호'에서 위치 서비스를 켜주세요.", preferredStyle: .alert)
+           let goSetting = UIAlertAction(title: "설정으로 이동", style: .destructive) { _ in
+               if let appSetting = URL(string: UIApplication.openSettingsURLString) {
+                   UIApplication.shared.open(appSetting)
+               }
+           }
+    //        let cancel = UIAlertAction(title: "취소", style: .default) { [weak self] _ in
+    //            async { await self?.reloadData }
+    //        }
+    //        requestLocationServiceAlert.addAction(cancel)
+           requestLocationServiceAlert.addAction(goSetting)
+           
+//           present(requestLocationServiceAlert, animated: true)
+       }
 }
