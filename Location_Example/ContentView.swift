03c2362912e8f80b2470a53af36052dc87a16bac
LeeeeSuHyeon <tngus0673@naver.com>
2024-02-07 22:30:23 +0900

    지도와 버튼 VStack 연결 후 AR on/off 가능

--- Location_Example/ContentView.swift
+++ Location_Example/ContentView.swift
@@ -47,38 +47,58 @@ struct ContentView: View {
     // 전역으로 CoreLocationEx 인스턴스 생성
     @ObservedObject var coreLocation = CoreLocationEx()
     
+    @State var isPresented: Bool = false
+    
     @State private var region = MKCoordinateRegion(
         center: CLLocationCoordinate2D(latitude: 0, longitude: 0),
         span: MKCoordinateSpan(latitudeDelta: 0.001, longitudeDelta: 0.001)
     )
     
     var body: some View {
-        Map(coordinateRegion: $region, showsUserLocation: true)
-            .onAppear {
-                // Map이 나타날 때 현재 위치를 표시
-                if let location = coreLocation.locationManager.location?.coordinate {
-                    region.center = location
-                }
-            }
-            .onChange(of: coreLocation.locationManager.location) { _ in
-                // 위치가 업데이트될 때 지도의 중심을 업데이트
-                if let location = coreLocation.locationManager.location?.coordinate {
-                    region.center = location
-                }
-            }
-            .edgesIgnoringSafeArea(.all)
-        
         VStack{
-            Text("위도 : \(coreLocation.locationManager.location?.coordinate.latitude ?? 0.0)")
-            Text("경도 : \(coreLocation.locationManager.location?.coordinate.longitude ?? 0.0)")
-            Text("고도 : \(coreLocation.locationManager.location?.altitude ?? 0.0)")
-            Text("수평 정확도 : \(coreLocation.locationManager.location?.verticalAccuracy ?? 0.0)")
-            Text("수직 정확도 : \(coreLocation.locationManager.location?.horizontalAccuracy ?? 0.0)")
+            Map(coordinateRegion: $region, showsUserLocation: true)
+                .onAppear {
+                    // Map이 나타날 때 현재 위치를 표시
+                    if let location = coreLocation.locationManager.location?.coordinate {
+                        region.center = location
+                    }
+                }
+                .onChange(of: coreLocation.locationManager.location) { _ in
+                    // 위치가 업데이트될 때 지도의 중심을 업데이트
+                    if let location = coreLocation.locationManager.location?.coordinate {
+                        region.center = location
+                    }
+                }
+                .edgesIgnoringSafeArea(.all)
+            
+            
+            Button {
+               isPresented.toggle()
+            } label: {
+               Label("View in AR", systemImage: "arkit")
+            }
+            .buttonStyle(BorderedProminentButtonStyle())
+            .padding(24)
+            
+            // 3.
+            .fullScreenCover(isPresented: $isPresented, content: {
+               ARTest(isPresented: $isPresented)
+            })
         }
-        .padding()
+        
     }
 }
 
 #Preview {
     ContentView()
 }
+
+
+//VStack{
+//    Text("위도 : \(coreLocation.locationManager.location?.coordinate.latitude ?? 0.0)")
+//    Text("경도 : \(coreLocation.locationManager.location?.coordinate.longitude ?? 0.0)")
+//    Text("고도 : \(coreLocation.locationManager.location?.altitude ?? 0.0)")
+//    Text("수평 정확도 : \(coreLocation.locationManager.location?.verticalAccuracy ?? 0.0)")
+//    Text("수직 정확도 : \(coreLocation.locationManager.location?.horizontalAccuracy ?? 0.0)")
+//}
+//.padding()
